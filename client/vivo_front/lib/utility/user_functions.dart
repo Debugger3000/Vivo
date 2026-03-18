551e7b2f63ac1b784685e74875afd5c53b52d694
Debugger3000 <145146370+Debugger3000@users.noreply.github.com>
2026-03-18 19:21:49 -0400

    marker info window updated

--- client/vivo_front/lib/utility/user_functions.dart
+++ client/vivo_front/lib/utility/user_functions.dart
@@ -1,7 +1,7 @@
 
 
 import 'package:supabase_flutter/supabase_flutter.dart';
-
+import 'package:intl/intl.dart';
 
 /// Returns the currently logged-in user's data, or `null` if not logged in.
 // Future<User?> getCurrentUserData() async {
@@ -14,3 +14,15 @@ Future<String?> getCurrentUserId() async {
   final user = Supabase.instance.client.auth.currentUser;
   return user?.id;
 }
+
+
+String formatEventTime(String? isoString) {
+      if (isoString == null || isoString.isEmpty) return 'TBD';
+      try {
+        DateTime dt = DateTime.parse(isoString).toLocal();
+        // Pattern: MMM (Month) d (Day), h:mm a (Hour:Min AM/PM)
+        return DateFormat('MMM d, h:mm a').format(dt);
+      } catch (e) {
+        return 'Invalid Date';
+      }
+    }
